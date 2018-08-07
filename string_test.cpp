3a4fda565cf223dc0b560407f7e3c21ee27178ee
Michal Schulz <michal.schulz@gmx.de>
2018-08-07 16:42:27 +0200

    more unit tests

--- string_test.cpp
+++ string_test.cpp
@@ -24,4 +24,92 @@ TEST_CASE("t_std::string class", "[t_std::string]") {
         CHECK( s6a == "xxxxxxxxxx" );
         CHECK( s6b == "**********" );
     }
+
+    SECTION("Testing operator=") {
+        t_std::string str1, str2, str3;
+        str1 = "Test string: "; // c-string
+        str2 = 'x';             // single character
+        str3 = str1 + str2;
+        CHECK( str1 == "Test string: " );
+        CHECK( str2 == "x" );
+        CHECK( str3 == "Test string: x" );
+    }
+
+    SECTION("Testing capacity functions") {
+        t_std::string str("Test string");
+
+        CHECK( str.size() == 11 );
+        CHECK( str.length() == 11 );
+
+        str = t_std::string("I like to code in C");
+        CHECK( str == "I like to code in C" );
+
+        unsigned sz = str.size();
+        str.resize(sz + 2, '+');
+        CHECK( str == "I like to code in C++" );
+
+        str.resize(14);
+        CHECK( str == "I like to code" );
+    }
+
+    SECTION("Element access") {
+        char str_contents[] = "Test string";
+        t_std::string str(str_contents);
+
+        for (int i = 0; i < str.length(); ++i)
+        {
+            CHECK( str[i] == str_contents[i] );
+            CHECK( str.at(i) == str_contents[i] );
+        }
+    }
+
+    SECTION("Modifiers") {
+        {
+            t_std::string name("John");
+            t_std::string family("Smith");
+            name += " K. "; // c-string
+            name += family; // string
+            name += '\n'; // character
+
+            CHECK( name == "John K. Smith\n" );
+        }
+
+        {
+            t_std::string str;
+            t_std::string str2 = "Writing ";
+            t_std::string str3 = "print 10 and then 5 more";
+
+            // used in the same order as described above:
+            str.append(str2);               // "Writing "
+            str.append(str3, 6, 3);         // "10 "
+            str.append("dots are cool", 5); // "dots "
+            str.append("here: ");           // "here: "
+            str.append(10u, '.');           // ".........."
+
+            CHECK( str == "Writing 10 dots here: .........." );
+        }
+        {
+            t_std::string str;
+            t_std::string base = "The quick brown fox jumps over a lazy dog.";
+
+            // used in the same order as described above:
+
+            str.assign(base);
+            CHECK( str == "The quick brown fox jumps over a lazy dog." );
+
+            str.assign(base, 10, 9);
+            CHECK( str == "brown fox" );
+
+            str.assign("pangrams are cool", 7);
+            CHECK( str == "pangram" );
+
+            str.assign("c-string");
+            CHECK( str == "c-string" );
+
+            str.assign(10, '*');
+            CHECK( str == "**********" );
+        }
+    }
+
+
 }
