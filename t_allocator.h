ac5dcb9267f7d3a0bb9d3c94bb989d65592b3e92
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-08-13 09:23:20 +0200

    allocator talks a little bit more

--- t_allocator.h
+++ t_allocator.h
@@ -4,6 +4,7 @@
 #define _T_ALLOCATOR_H
 
 #include <stdio.h>
+#include <typeinfo>
 #include "support.h"
 
 namespace t_std {
@@ -20,13 +21,13 @@ public:
     typedef uintptr_t           size_type;
     typedef ptrdiff_t           difference_type;
 
-    allocator() : construct_count(0), deconstruct_count(0), allocate_count(0), deallocate_count(0) {};
+    allocator() : construct_count(0), deconstruct_count(0), allocate_count(0), deallocate_count(0) { printf("Allocator @ %p for type %s\n", (void*)this, typeid(T).name()); };
     allocator(const allocator&) : allocator() {};
-    ~allocator() { printf("Constructed: %d, Deconstructed: %d\nAllocated: %d, Deallocated: %d\n", construct_count, deconstruct_count, allocate_count, deallocate_count); }
+    ~allocator() { printf("Destroying allocator @ %p for type %s\n", (void*)this, typeid(T).name()); printf("Constructed: %d, Deconstructed: %d\nAllocated: %d, Deallocated: %d\n", construct_count, deconstruct_count, allocate_count, deallocate_count); }
     pointer address(reference x) { return &x; }
     const_pointer address(const_reference x) { return &x; }
-    pointer allocate(size_type n) { pointer p = (pointer)AllocMem(n * sizeof(value_type), MEMF_CLEAR); printf("t_std::allocate(%d)=%p\n", (int)n, (void*)p); allocate_count++; return p; }
-    void deallocate(pointer p, size_type n) { printf("t_std::deallocate(%p, %d)\n", (void*)p, (int)n); if (n>0 && p!=nullptr) FreeMem((void*)p, n * sizeof(T)); deallocate_count++; }
+    pointer allocate(size_type n) { pointer p = (pointer)AllocMem(n * sizeof(value_type), MEMF_CLEAR); printf("t_std::allocate(%p, %d)=%p\n", (void*)this, (int)n, (void*)p); allocate_count++; return p; }
+    void deallocate(pointer p, size_type n) { printf("t_std::deallocate(%p, %p, %d)\n", (void*)this, (void*)p, (int)n); if (n>0 && p!=nullptr) FreeMem((void*)p, n * sizeof(T)); deallocate_count++; }
     size_type max_size() { return (size_type)-1 / sizeof(value_type); }
     void construct(pointer p, const_reference val) { new((void*)p) value_type(val); construct_count++;}
     void destroy(pointer p) { p->~value_type(); deconstruct_count++; }
