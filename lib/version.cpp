0c221406b051eba3ea7774c78f18699e337f8357
Michal Schulz <michal.schulz@gmx.de>
2020-04-01 12:04:59 +0200

    build entire $VER string in cmake

--- lib/version.cpp
+++ lib/version.cpp
@@ -1,5 +1,5 @@
 namespace tinystd {
     namespace __internal__ {
-        const char * __attribute__((used)) __tinystd_version = "\0$VER: tiny-stl " VERSION_STRING;
+        const char * __attribute__((used)) __tinystd_version = "\0" VERSION_STRING;
     }
 }
