f39123b49bc9ed4122161c08616e8610035b641a
Michal Schulz <michal.schulz@gmx.de>
2020-04-05 21:39:18 +0200

    Allow to override default printf with BUG macro

--- lib/memory.cpp
+++ lib/memory.cpp
@@ -13,6 +13,10 @@
 #include <strings.h>
 #include <tinystl/bits/support.h>
 
+#ifndef BUG
+#define BUG(...) printf(__VA_ARGS__)
+#endif
+
 void * mungwall_malloc(size_t size)
 {
     uint32_t *ptr = reinterpret_cast<uint32_t *>(malloc(size + 32));
@@ -36,16 +40,23 @@ void mungwall_free(void *ptr, size_t size)
     uint32_t *p = reinterpret_cast<uint32_t *>(ptr);
 
     p -= 4;
-    if (*p != (unsigned int)size)
-        printf("Size mismatch at mungwall_free!! %d != %d\n", *p, (uint32_t)size);
+    if (*p != (unsigned int)size) {
+#ifndef NDEBUG
+        BUG("Size mismatch at mungwall_free!! %d != %d\n", *p, (uint32_t)size);
+#endif
+    }
 
     if (p[1] != 0xdeadbeef || p[2] != 0xdeadbeef || p[3] != 0xdeadbeef)
     {
-        printf("mungwall_free(): left wall damaged %08x%08x%08x\n", p[1], p[2], p[3]);
+#ifndef NDEBUG
+        BUG("mungwall_free(): left wall damaged %08x%08x%08x\n", p[1], p[2], p[3]);
+#endif
     }
     if (p[4 + size/4] != 0xcafebabe || p[5+size/4] != 0xcafebabe || p[6+size/4] != 0xcafebabe || p[7+size/4] != 0xcafebabe)
     {
-        printf("mungwall_free(): right wall damaged %08x%08x%08x%08x\n", p[4 + size/4], p[5+size/4], p[6+size/4],p[7+size/4]);
+#ifndef NDEBUG
+        BUG("mungwall_free(): right wall damaged %08x%08x%08x%08x\n", p[4 + size/4], p[5+size/4], p[6+size/4],p[7+size/4]);
+#endif
     }
 
     free(p);
